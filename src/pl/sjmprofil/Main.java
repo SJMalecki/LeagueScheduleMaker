6a2a3dd1f0ef71945d67e5dadd84f5574e508030
sjmalecki <sebastian.jacek.malecki@gmail.com>
2018-11-04 23:49:09 +0100

    Main class for tests

--- src/pl/sjmprofil/Main.java
+++ src/pl/sjmprofil/Main.java
@@ -3,6 +3,16 @@ package pl.sjmprofil;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Team team = new Team("Breslauer Krähe");
+	    team.addContestant(new PlatiniumContestant("Juan Pablo Montoya"));
+	    team.addContestant(new PlatiniumContestant("Mark Webber"));
+
+	    team.printSquad();
+
+	    Team team2 = new Team("Poznań Krokodile");
+	    team2.printSquad();
+	    
+	    team.matchResult(team2, 1,0);
+
     }
 }
